6cdc46cdc69619c63c3c6bc121dfc08cefe203e9
chadam17 <102019635+chadam17@users.noreply.github.com>
2024-04-14 10:12:28 +0200

    Add files via upload

---
+++ articles.js
@@ -0,0 +1,43 @@
+let infoArticles = {
+  wear: [
+    {
+      id: 1,
+      brand: 'Ralph Lauren',
+      type: 'polo',
+      sales: 81273,
+      color: 'white'
+    },
+    {
+      id: 2,
+      brand: 'Giorgio Armani',
+      type: 'tracksuit',
+      sales: 72812,
+      color: 'black'
+    },
+    {
+      id: 3,
+      brand: 'Donatella Versace',
+      type: 'dress',
+      sales: 72188,
+      color: 'green'
+    }
+  ],
+  sports: [
+    {
+      id: 1,
+      name: 'Football',
+      players: 11,
+      ranking: 'high',
+      level: 'basic'
+    },
+    {
+      id: 2,
+      name: 'Basketball',
+      players: 5,
+      ranking: 'medium',
+      level: 'advanced'
+    }
+  ]    
+}
+
+module.exports.infoArticles = infoArticles;
