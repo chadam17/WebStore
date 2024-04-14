abe02536febab7b698e6d2b5e1f6358aaee7952e
chadam17 <102019635+chadam17@users.noreply.github.com>
2024-04-14 09:52:59 +0200

    Add files via upload
    Minimal WebStore written with Node.JS and Express Framework

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+//Import articles from file.
+const {infoArticles} = require('./datos/articles.js');
+
+// Routers
+const routerWear = require('./routers/wear.js');
+app.use('/api/articles/wear', routerWear);
+
+const routerSports = require('./routers/sports.js');
+app.use('/api/articles/sports', routerSports);
+
+// Routing
+app.get('/', (req, res) => {
+  res.send('WebStore Express Server');        
+});
+
+app.get('/api/articles', (req, res) => {
+  res.json(infoArticles);
+});
+
+const PUERTO = process.env.PORT || 3000;
+
+app.listen(PUERTO, () => {
+  console.log(`Server is listening at port: ${PUERTO}...`);      
+});
